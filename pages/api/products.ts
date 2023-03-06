717a4d2b859c01e587d466ee1aac4dea38786e31
PatricioEtcheverry <patricioetcheverry@gmail.com>
2023-03-06 19:51:25 -0300

    Primer commit

---
+++ pages/api/products.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { products } from "./db";
+
+export default function handler(req : NextApiRequest, res : NextApiResponse<typeof products>) {
+    res.status(200).json(products)
+  }
